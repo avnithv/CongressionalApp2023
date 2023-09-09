75ccbf849f1bf82b4a567f0e3673bf1b46d207ff
AryanG1111 <115119853+AryanG1111@users.noreply.github.com>
2023-09-09 14:23:08 -0400

    Add files via upload
    added chatbot files

---
+++ congressional_app/lib/chat_screen.dart
@@ -0,0 +1,128 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import 'chatmessage.dart';
+import 'threedots.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final List<ChatMessage> _messages = [];
+  late OpenAI? chatGPT;
+
+  bool _isTyping = false;
+
+  @override
+  void initState() {
+    chatGPT = OpenAI.instance.build(
+        token: "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",
+        baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 30)));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  // Link for api - https://beta.openai.com/account/api-keys
+
+  void _sendMessage() async {
+    if (_controller.text.isEmpty) return;
+    ChatMessage message = ChatMessage(
+      text: _controller.text,
+      sender: "user",
+      isImage: false,
+    );
+
+    setState(() {
+      _messages.insert(0, message);
+      _isTyping = true;
+    });
+
+    _controller.clear();
+
+    final request = CompleteText(
+        prompt: message.text, model: TextDavinci3Model(), maxTokens: 200);
+
+    final response = await chatGPT!.onCompletion(request: request);
+    Vx.log(response!.choices[0].text);
+    insertNewData(response.choices[0].text, isImage: false);
+  }
+
+  void insertNewData(String response, {bool isImage = false}) {
+    ChatMessage botMessage = ChatMessage(
+      text: response,
+      sender: "bot",
+      isImage: isImage,
+    );
+
+    setState(() {
+      _isTyping = false;
+      _messages.insert(0, botMessage);
+    });
+  }
+
+  Widget _buildTextComposer() {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _controller,
+            onSubmitted: (value) => _sendMessage(),
+            decoration: const InputDecoration.collapsed(
+                hintText: "Question/description"),
+          ),
+        ),
+        ButtonBar(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                _sendMessage();
+              },
+            ),
+          ],
+        ),
+      ],
+    ).px16();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("ChatBBC")),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Flexible(
+                  child: ListView.builder(
+                reverse: true,
+                padding: Vx.m8,
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  return _messages[index];
+                },
+              )),
+              if (_isTyping) const ThreeDots(),
+              const Divider(
+                height: 1.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: context.cardColor,
+                ),
+                child: _buildTextComposer(),
+              )
+            ],
+          ),
+        ));
+  }
+}
