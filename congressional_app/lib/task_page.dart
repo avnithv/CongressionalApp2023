1347a41465df0411cd41e5515aaa4faebb772bb3
kanisiva2 <139166086+kanisiva2@users.noreply.github.com>
2023-11-01 17:44:24 -0400

    Update task_page.dart

--- congressional_app/lib/task_page.dart
+++ congressional_app/lib/task_page.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 import 'main.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:path/path.dart' as p;
 import 'dart:io';
+import 'package:cupertino_icons/cupertino_icons.dart';
 
 class ListItem {
   String todoText;
@@ -56,12 +60,12 @@ class _TasksWidgetState extends State<TasksWidget> {
   var popUpTextController = TextEditingController();
   late TextEditingController controller;
   late TextEditingController coinController;
-  final String pth = "/Users/avnith/Desktop/Congressional App Challenge/app/CongressionalApp2023/congressional_app";
 
   List<ListItem> WidgetList = [];
 
   void readTasks() {
-    File file = File(p.join(pth, 'lib', 'tasks.txt'));
+    String pth = "/Users/kanisiva/Documents/VSCode/CongressionalApp/CongressionalApp2023/congressional_app/lib/tasks.txt";
+    File file = File(pth);
     List<String> strs = file.readAsLinesSync();
     for (String i in strs) {
       if (i != "") {
@@ -71,7 +75,8 @@ class _TasksWidgetState extends State<TasksWidget> {
   }
 
   void writeTasks() {
-    File file = File(p.join(pth, 'lib', 'tasks.txt'));
+    String pth = "/Users/kanisiva/Documents/VSCode/CongressionalApp/CongressionalApp2023/congressional_app/lib/tasks.txt";
+    File file = File(pth);
     file.writeAsStringSync('');
     for (ListItem i in WidgetList) {
       file.writeAsStringSync(i.getValue() + "\n", mode: FileMode.append);
@@ -100,38 +105,37 @@ class _TasksWidgetState extends State<TasksWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Task List", style: TextStyle(color: Theme.of(context).colorScheme.primary, fontSize: 45,)),
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
+      appBar: appBar(),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(24),
-            child: TextField(
-              decoration: InputDecoration(hintText: "Enter a New Task"),
-              style: TextStyle(
-                fontSize: 22.0,
-              ),
-              controller: textController,
-              cursorWidth: 5.0,
-              autocorrect: true,
-              autofocus: true,
-            ),
-          ),
-          ElevatedButton(
-            child: Text("Add Task"),
-            onPressed: () {
-              if (textController.text.isNotEmpty) {
-                WidgetList.add(new ListItem(textController.text, false));
-                setState(() {
-                  textController.clear();
-                });
-              }
-            },
-          ),
+          _searchField(),
+          const SizedBox(height: 30),
+          // Container(
+          //   margin: EdgeInsets.all(24),
+          //   child: TextField(
+          //     decoration: InputDecoration(hintText: "Enter a New Task"),
+          //     style: TextStyle(
+          //       fontSize: 22.0,
+          //     ),
+              // controller: textController,
+              // cursorWidth: 5.0,
+              // autocorrect: true,
+              // autofocus: true,
+          //   ),
+          // ),
+          // ElevatedButton(
+          //   child: Text("Add Task"),
+          //   onPressed: () {
+          //     if (textController.text.isNotEmpty) {
+          //       WidgetList.add(new ListItem(textController.text, false));
+          //       setState(() {
+          //         textController.clear();
+          //       });
+          //     }
+          //   },
+          // ),
           Expanded(
             child: ReorderableListView(
               children: <Widget>[
@@ -149,7 +153,6 @@ class _TasksWidgetState extends State<TasksWidget> {
                           if (res != null && res[0] == MyHomePage.passwode) {
                             MyHomePage.coins += int.parse(res[1]);
                             WidgetList.remove(widget);
-                            MyHomePage.hompate!.setState(() {});
                             setState(() {});
                           }
                           // setState(() {
@@ -194,6 +197,7 @@ class _TasksWidgetState extends State<TasksWidget> {
                       movementDuration: const Duration(milliseconds: 200),
                       onDismissed: (dismissDirection) {
                         setState(() {
+                          // Remove the Dismissible widget from the WidgetList
                           WidgetList.remove(widget);
                         });
                         Fluttertoast.showToast(msg: "Item Deleted!");
@@ -284,7 +288,138 @@ class _TasksWidgetState extends State<TasksWidget> {
     );
   }
 
-  void confirm() {
+  
+  AppBar appBar() {
+    return AppBar(
+        title: Text(
+          'Tasks',
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        leading: GestureDetector(
+          onTap: () {
+            
+          },
+            child: Container(
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
+              child: SvgPicture.asset(
+                'assets/icons/Arrow - Left 2.svg',
+                height: 20,
+                width: 20,
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10)
+              ),
+            ),
+        ),
+        actions: [
+          Text(
+            '${MyHomePage.coins}',
+            style: TextStyle(
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          GestureDetector(
+              onTap: () {
+
+              },
+              child: Container(
+                margin: EdgeInsets.only(top: 10, bottom: 10, right: 10),
+                alignment: Alignment.center,
+                width: 37,
+                child: SvgPicture.asset(
+                  'assets/icons/money.svg',
+                  height: 30,
+                  width: 30,
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xffF7F8F8),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+            ),
+          ),
+        ], 
+      );
+}
+
+Container _searchField() {
+    return Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff1D1617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0
+                )
+              ]
+            ),
+            child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: EdgeInsets.all(15),
+                  hintText: 'Add A Task!',
+                  hintStyle: TextStyle(
+                    color: Color(0xffDDDADA),
+                    fontSize: 14
+                  ),
+
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                        if (textController.text.isNotEmpty) {
+                        WidgetList.add(new ListItem(textController.text, false));
+                        setState(() {
+                          textController.clear();
+                        });
+                      }
+                    },
+                    child: Container(
+                      width: 100,
+                      child: IntrinsicHeight(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            VerticalDivider(
+                              color: Colors.black,
+                              indent: 10,
+                              endIndent: 10,
+                              thickness: 0.1,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(10),
+                                child: Icon(
+                                  CupertinoIcons.arrow_right_square,
+                                  size: 30,
+                                ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide.none
+                  )
+                ),
+                controller: textController,
+                cursorWidth: 5.0,
+                autocorrect: true,
+                autofocus: true,
+              )
+          );
+  }
+
+void confirm() {
     Navigator.of(context).pop([controller.text, coinController.text]);
     controller.clear();
   }
